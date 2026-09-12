fce2a31e55d8f2ea71a60f70ebd710d8e1bb5119
Arun Kumar M <arunkm158@gmail.com>
2026-09-12 11:32:53 +0530

    fix missing useState import in Resources.tsx

--- src/pages/Resources.tsx
+++ src/pages/Resources.tsx
@@ -1,4 +1,4 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import { SEO } from "@/components/SEO";
 import { Button } from "@/components/ui/button";
